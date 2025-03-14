760439ae82baa4e2c2c0a7207074f9bd39d52ae2
di0rio <caua.diorio29@gmail.com>
2025-03-14 16:33:37 -0300

    header finalizado

--- FRONT-END/dia1/src/components/Header/Header.jsx
+++ FRONT-END/dia1/src/components/Header/Header.jsx
@@ -1,13 +1,43 @@
-import styles from './Header.module.css'
+import styles from "./Header.module.css";
+
+import { Link } from "react-router-dom";
+
+import logo from "../../assets/logo.png";
+import casaVerde from "../../assets/casaVerde.png";
+
+const navigation = [
+  { componente: "/howto", name: "Como fazer" },
+  { componente: "/offers", name: "Ofertas" },
+  { componente: "/testimonials", name: "Depoimentos" },
+  { componente: "/videos", name: "Vídeos" },
+  { componente: "/mycart", name: "Meu carrinho" },
+];
 
 const Header = () => {
   return (
-    <div>
-      <div>
-        
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.conjuntoLogo}>
+          <img src={logo} alt="Logo Casa Verde" />
+          <img src={casaVerde} alt="Casa Verde" />
+        </div>
+        <nav className={styles.nav}>
+          <ul className={styles.navList}>
+            {navigation.map((nav, index) => (
+              <li key={nav.name} className={styles.navItem}>
+                <Link to={nav.componente} className={styles.navLink}>
+                  {nav.name}
+                </Link>
+                {index < navigation.length - 1 && (
+                  <span className={styles.divider}>/</span>
+                )}
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
-    </div>
-  )
-}
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- FRONT-END/dia1/src/pages/App.jsx
+++ FRONT-END/dia1/src/pages/App.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../routes/Home/Home";
 import HowTo from "../routes/HowTo/HowTo";
 import Offers from "../routes/Offers/Offers";
+import Testimonials from "../routes/Testimonials/Testimonials";
+import Videoo from "../routes/Videoo/Videoo";
+import MyCart from "../routes/MyCart/MyCart";
 
 const App = () => {
   return (
@@ -12,7 +15,9 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/howto" element={<HowTo />} />
         <Route path="/offers" element={<Offers />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/testimonials" element={<Testimonials />} />
+        <Route path="/videos" element={<Videoo />} />
+        <Route path="/mycart" element={<MyCart />} />
       </Routes>
     </BrowserRouter>
   );
