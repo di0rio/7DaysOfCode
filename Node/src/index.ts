ea0ffea537bc27dbf5e74b3510e3b162793bed5c
di0rio <caua.diorio29@gmail.com>
2025-03-25 09:47:35 -0300

    comentando para entender o código

--- Node/src/index.ts
+++ Node/src/index.ts
@@ -5,3 +5,18 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+//DIA1
+
+// import express from 'express';
+
+// const app = express();
+// const port = 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello, world!');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
