f3ce0b2127df3cea87d2a4274eba9823cce1f4b9
di0rio <caua.diorio29@gmail.com>
2025-03-25 09:45:52 -0300

    realizando desafio dia 3 mas nao tenho acesso a api pois nao tenho 18 anos

--- Node/src/index.ts
+++ Node/src/index.ts
@@ -1,11 +1,6 @@
-import express from 'express';
+import { app } from "./app";
 
-const app = express();
 const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
