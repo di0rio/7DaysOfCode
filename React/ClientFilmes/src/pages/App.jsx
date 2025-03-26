38ae93fe5eef70c24986451752543f1654ecb4f6
di0rio <caua.diorio29@gmail.com>
2025-03-26 17:37:19 -0300

    iniciando desafio de filmes populares alura

--- React/ClientFilmes/src/pages/App.jsx
+++ React/ClientFilmes/src/pages/App.jsx
@@ -2,16 +2,12 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "../routes/Home/Home";
-import SignIn from "../routes/SignIn/SignIn";
-import SignUp from "../routes/SignUp/SignUp";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
